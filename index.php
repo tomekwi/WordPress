99f59181e9d89aa7bace7c654104193a75c56bdb
Tomasz Wiszniewski <twiszniewski@gmail.com>
2013-09-24 06:29:56 +0200

    Composer.json finished

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 /**
- * This file is a proxy redirecting the user to the admin panel.
+ * This is the WordPress core served as a Composer package. It's stripped of
+ * the wp-content folder - all its glory (themes, plugins, languages, etc.)
+ * is supposed to be put into '/' or '/directory-of-your-choice' for you to
+ * enjoy a clear folder structure.
+ * 
+ * The composer package is fitted with composer/installers, so you can specify
+ * a custom installation path (for example '/wordpress-core'). For more
+ * information about custom installers visit https://github.com/composer/installers .
  * 
  * @author Tomasz Wiszniewski <twiszniewski@gmail.com>
+ */
+
+/**
+ * This file is a proxy redirecting the user to the admin panel.
  */
 
 header('Location: wp-admin/');
